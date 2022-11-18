0e94b35d0ca6473273530db582720c54c8f2e100
Yasas Bulumulla <yasasb@mobitel.lk>
2022-11-18 19:48:30 +0530

    Initial commit

---
+++ src/main/java/com/javahungry/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.javahungry.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path="/")
+	public String sayHello() {
+		return "Hello!! from Java HunYT";
+	}
+
+}
